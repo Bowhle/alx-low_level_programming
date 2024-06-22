8de3d80448e245fffe66ec474a925d8bf9f910e4
“Bowhle” <bowhlem@gmail.com>
2024-06-22 14:02:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <studio.h>
 /**
 * main - prints "Programming is like building a multilingual puzzle"
@@ -8,5 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
