a52f9f6fabaaed9e6b027e64abefe5c1fdc12a8d
“Bowhle” <bowhlem@gmail.com>
2024-06-23 12:13:25 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,16 @@
 int main(void)
 
 {
+
 	char low;
 
-	for  (low= 'a'; low <= 'z'; low++)
+	for  (low = 'a'; low <= 'z'; low++)
 	putchar(low);
 
 	for (low = 'A'; low <= 'Z'; low++)
 	putchar(low);
-	putchar('/n');
+	putchar("/n");
+
 	retun (0);
 
 }
