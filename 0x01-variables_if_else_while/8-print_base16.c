20e6485b555b2070a7b2175d13e242e540e1e4ab
“Bowhle” <bowhlem@gmail.com>
2024-06-24 13:14:19 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,11 +12,13 @@ int main(void)
 	c = 'a';
 	d = 0;
 	while
-		(d < 10)
-		putchar (d + '0'); d++;
+		(d < 10) {
+			putchar (d + '0'); d++;
+		}
 	while
-		(c <= 'f')
-		putchar(c);
+		(c <= 'f') {
+			putchar(c); c++;
+		}
 	putchar('\n');
 	return (0);
 }
