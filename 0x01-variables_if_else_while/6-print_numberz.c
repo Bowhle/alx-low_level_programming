70cff121d4443bd5395cb8d2b69617ff8c89ccae
“Bowhle” <bowhlem@gmail.com>
2024-06-24 11:47:15 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,19 +2,8 @@
 
 int main(void)
 {
-    putchar('0');
-    putchar('1');
-    putchar('2');
-    putchar('3');
-    putchar('4');
-    putchar('5');
-    putchar('6');
-    putchar('7');
-    putchar('8');
-    putchar('9');
-    
-    putchar('\n');  // Print a newline character at the end
-    
-    return 0;
+	for (int i = 0; i < 10; i++)
+	putchar(i + '0')
+	putchar('\n');
+	return (0);
 }
-
