2eaaf93bccd5a0c25a7f49ff35d46c78a6936898
“Bowhle” <bowhlem@gmail.com>
2024-06-26 15:45:52 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 
 /**
- * this is text
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: The number to start from
+ * Return: Always 0.
  */
 
 void print_to_98(int n)
