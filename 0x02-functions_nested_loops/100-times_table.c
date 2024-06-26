094ae9a7c622f1689c1e5d39845ce72e26479053
“Bowhle” <bowhlem@gmail.com>
2024-06-26 16:50:32 -0700

    crash recovery

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,55 +1,40 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the n times table, starting with 0
+ * print_times_table - Prints the times table, starting with 0
  * @n: The value of the times table to print
- * If n is greater than 15 or less than 0, the function should not print anything.
  */
 
 void print_times_table(int n)
 {
-	int i, j, result;
+        int num, mult, prod;
 
-	if (n < 0 || n > 15)
-	return;
-
-	for (i = 0; i <= n; i++)
-	{
-	for (j = 0; j <= n; j++)
-	{
-	result = i * j;
-	if (j != 0)
-	{
-	_putchar(',');
-	_putchar(' ');
-
-	if (result < 10)
-	{
-	_putchar(' ');
-	_putchar(' ');
-	}
-	else if (result < 100)
-	{
-	_putchar(' ');
-	}
-	}
-
-	if (result >= 100)
-	{
-	_putchar((result / 100) + '0');
-	_putchar(((result / 10) % 10) + '0');
-	_putchar((result % 10) + '0');
-	}
-	else if (result >= 10)
-	{
-	_putchar((result / 10) + '0');
-	_putchar((result % 10) + '0');
-	}
-	else
-	{
-	_putchar(result + '0');
-	}
-	}
-	_putchar('\n');
-	}
+        if (n >= 0 && n <= 15)
+        {
+        for (num = 0; num <= n; num++)
+        {
+        _putchar('0');
+        for (mult = 1; mult <= n; mult++)
+        {
+        _putchar(',');
+        _putchar(' ');
+        prod = num * mult;
+        if (prod <= 99)
+        _putchar(' ');
+        if (prod <= 9)
+        _putchar(' ');
+        if (pro >= 100)
+        {
+        _putchar((prod / 100) + '0');
+        _putchar(((prod / 10) % 10) + '0');
+        }
+        else if (prod <= 99 && prod >= 10)
+        {
+        _putchar((prod / 10) + '0');
+        }
+        _putchar((prod % 10) + '0');
+        }
+        _putchar('\n');
+        }
+        }
 }
