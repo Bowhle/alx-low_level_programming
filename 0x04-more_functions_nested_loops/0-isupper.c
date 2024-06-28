3d12e2d6854c5c617fdbf32aa4867335e0f57e43
“Bowhle” <bowhlem@gmail.com>
2024-06-28 11:00:44 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,17 @@
 /**
  * _isupper - checks for uppercase character
  * @c: the character to check
- *
- * Return: 1 if c is uppercase, 0 otherwise
+ * Return: Always 0
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z)'
+	{
 	return (1);
+	}
 	else
+	{
 	return (0);
+	}
 }
