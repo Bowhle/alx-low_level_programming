87bddcfb89a93f92c85505048e7f6e2a27d6058a
“Bowhle” <bowhlem@gmail.com>
2024-07-14 12:53:29 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,50 +1,28 @@
 #include "main.h"
 /**
- * long_1 - Main Entry
- * @s: input
+ * wildcmp - Main Entry
+ * @s1: input
+ * @s2: input
  * Return: 0
  */
-
-int long_1(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	if (*s != '\0')
+	if (*s1 == '\0')
 	{
-		return (1 + long_1(s + 1));
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
-
-}
-/**
- * compare - Main Entry
- * @s: input
- * @l: input
- * Return: 0
- */
-int compare(char *s, int l)
-{
-	if (l <= 0)
-	{
-		return (1);
-	}
-	if (*s == *(s + (l - 1)))
-	{
-		return (compare(s + 1, l - 2));
-	}
-	else
-	{
-		return (0);
-	}
-}
-
-/**
- * is_palindrome - Main Entry
- * @s: input
- * Return: 0
- */
-int is_palindrome(char *s)
-{
-	int l;
-
-	l = long_1(s);
-	return (compare(s, l));
 }
