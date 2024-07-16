6ee860b198cfc6261d565a4d916ce20ac0c7e3e5
“Bowhle” <bowhlem@gmail.com>
2024-07-16 13:33:06 -0700

    try again

--- 0-whatsmyname.c
+++ 0-whatsmyname.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main - is a function that prints the name of the file
- * @argc: is an argument counter for char
- * @argv: is an argument value for char
+ * main - is a function that prints the name of the program
+ * @argc: is an argument count
+ * @argv: is an argument vector
  * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[argc - argc]);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
