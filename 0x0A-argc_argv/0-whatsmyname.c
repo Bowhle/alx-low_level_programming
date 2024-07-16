a7fecbaf25f7854c070236aaa1cf37dc57ab9cf7
“Bowhle” <bowhlem@gmail.com>
2024-07-16 13:38:19 -0700

    try again

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - is a function that prints the name of the program
+ * @argc: is an argument count
+ * @argv: is an argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
