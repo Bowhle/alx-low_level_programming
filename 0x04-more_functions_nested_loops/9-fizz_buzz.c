807d1f16289158418c34000beeaed35025869ad2
“Bowhle” <bowhlem@gmail.com>
2024-07-17 10:21:31 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,22 +11,27 @@ int main(void)
 {
 	int num;
 
-	for (num = 1; num <= 100; i++)
+	for (num = 1; num < 100; i++)
 	{
 	if ((num % 3) == 0 && (num % 5) == 0)
+	{
 	printf("FizzBuzz");
-	
-	else if ((num % 3) == 0)
+	}
+	else if (num % 3 == 0)
+	{
 	printf("Fizz");
-	
-	else if ((num % 5) == 0)
+	}
+	else if (num % 5 == 0)
+	{
 	printf("Buzz");
-	
+	}
 	else
+	{
 	printf("%d", num);
-
+	}
 	if (num == 100)
 	continue;
+	{
 	printf(" ");
 	}
 	printf("\n");
