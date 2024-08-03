64954217481198c73224136a3a28f79a6e415423
bowhlem <bowhlem@gmail.com>
2024-08-03 05:51:32 -0700

    betty style is always right

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,38 +1,56 @@
 #include "lists.h"
-/**
- * add_node_end - Add a new node at the end of a list
- * @head: Address of the first node of a list
- * @str: Address of the string to insert into the new node
- * Return: Address of the new node
- **/
 
+/**
+ * add_node_end - Adds a new node at the end of a list_t list.
+ * @head: Address of the pointer to the first node of the list.
+ * @str: The string to insert into the new node.
+ *
+ * Return: Address of the new node, or NULL if it failed.
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *temp, *temp2;
+	list_t *new_node, *last_node;
 	unsigned int length = 0;
+	char *str_dup;
+
 	if (str == NULL)
-	return (NULL);
-	temp = malloc(sizeof(list_t));
-	if (temp == NULL)
-	return (NULL);
-	temp->str = strdup(str);
-	if (temp->str == NULL)
 	{
-	free(temp);
 	return (NULL);
 	}
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+	return (NULL);
+	}
+
+	str_dup = strdup(str);
+	if (str_dup == NULL)
+	{
+	free(new_node);
+	return (NULL);
+	}
+
 	while (str[length])
+	{
 	length++;
-	temp->len = length;
-	temp->next = NULL;
+	}
+	new_node->str = str_dup;
+	new_node->len = length;
+	new_node->next = NULL;
+
 	if (*head == NULL)
 	{
-	*head = temp;
-	return (temp);
+	*head = new_node;
+	return (new_node);
 	}
-	temp2 = *head;
-	while (temp2->next)
-	temp2 = temp2->next;
-	temp2->next = temp;
-	return (temp);
+
+	last_node = *head;
+	while (last_node->next)
+	{
+	last_node = last_node->next;
+	}
+	last_node->next = new_node;
+
+	return (new_node);
 }
