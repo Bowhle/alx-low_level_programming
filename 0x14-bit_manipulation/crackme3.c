7acc3752beefaee84841fe1f61122ec5b5aa26a1
bowhlem <bowhlem@gmail.com>
2024-08-13 21:39:59 -0700

    Crackme3

---
+++ 0x14-bit_manipulation/crackme3.c
@@ -0,0 +1,6 @@
+#include <stddef.h>
+#include <stdio.h>                                                                                                                                                                                          int main(void)
+{
+        printf("This is the crackme program!\n");
+        return 0;
+}
