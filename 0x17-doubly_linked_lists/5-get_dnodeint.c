cfabfc44964634aeb3df1dab86b4e1f323939199
bowhle <bowhlem@gmailcom>
2024-09-22 04:40:13 -0700

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,15 +10,14 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int count = 0;
+	unsigned int count = 0;
 
-    while (head)
-    {
-        if (count == index)
-            return (head);
-        head = head->next;
-        count++;
-    }
-    return (NULL); /* If the node does not exist */
+	while (head)
+	{
+		if (count == index)
+			return (head);
+		head = head->next;
+		count++;
+	}
+	return (NULL); /* If the node does not exist */
 }
-
