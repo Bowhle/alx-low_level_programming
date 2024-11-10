325f5ba28f7a27fc55f30f569b6329d7abaa5c66
bowhle <bowhlem@gmailcom>
2024-11-10 07:20:10 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/operations.h
@@ -0,0 +1,50 @@
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+/**
+ * add - Adds two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the addition.
+ */
+int add(int a, int b);
+
+/**
+ * sub - Subtracts two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the subtraction.
+ */
+int sub(int a, int b);
+
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b);
+
+/**
+ * div - Divides two integers.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The result of the division.
+ */
+int div(int a, int b);
+
+/**
+ * mod - Computes the remainder of the division of two integers.
+ * @a: The numerator.
+ * @b: The denominator.
+ *
+ * Return: The remainder of the division.
+ */
+int mod(int a, int b);
+
+#endif /* OPERATIONS_H */
+
